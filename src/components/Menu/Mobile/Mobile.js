84c87425df5c0f5d671843aa9d7959c989128433
illia <illia@pdx.edu>
2020-10-06 13:04:15 -0700

    updates to mobile menu

--- src/components/Menu/Mobile/Mobile.js
+++ src/components/Menu/Mobile/Mobile.js
@@ -1,17 +1,53 @@
 import React, { useState } from 'react';
 import Drawer from '@material-ui/core/Drawer';
 import { makeStyles } from "@material-ui/styles";
+import { Link } from 'react-router-dom';
+import Divider from '@material-ui/core/Divider';
 
 //Components
 import { Navbar } from 'components';
-import { NavbarButton } from 'components';
 
 //Icon
 import MenuIcon from 'assets/menu.svg';
+import HomeIcon from 'assets/home.svg';
+import AboutIcon from 'assets/about.svg';
+import PortfolioIcon from 'assets/portfolio.svg';
+import ContactIcon from 'assets/email.svg';
 
 const useStyles = makeStyles(theme => ({
+    logo: {
+        fontSize: 30,
+        color: 'white'
+    },
     options: {
-        width: 250
+        width: 250,
+        height: '100%',
+        background: 'black',
+        display: 'flex',
+        flexDirection: 'column'
+    },
+    button: {
+        display: 'flex',
+        flexDirection: 'row',
+        padding: 15,
+        color: 'white',
+        fontSize: 16,
+        textDecoration: 'none',
+        '&:nth-child(1)': {
+            paddingTop: 30
+        }
+    },
+    buttonIconWrapper: {
+        display: 'flex',
+        alignItems: 'center'
+    },
+    buttonTextWrapper: {
+        display: 'flex',
+        alignItems: 'center',
+        paddingLeft: 15
+    },
+    dividerRoot: {
+        background: 'rgba(255,255,255,0.5)'
     }
 }))
 
@@ -20,20 +56,42 @@ const Desktop = ({ options=[] }) => {
     const [opened, setOpened] = useState(false);
 
     const toggleDrawer = (setTo) => {
-        setOpened(setTo)
+        setOpened(!opened)
+    }
+
+    const button = (index, path, name) => {
+        let icon = MenuIcon;
+        if(name.toUpperCase() === 'HOME') icon = HomeIcon;
+        if(name.toUpperCase() === 'ABOUT') icon = AboutIcon;
+        if(name.toUpperCase() === 'PORTFOLIO') icon = PortfolioIcon;
+        if(name.toUpperCase() === 'CONTACT') icon = ContactIcon;
+
+        return (
+            <Link key={index} to={path} className={classes.button} >
+                <div className={classes.buttonIconWrapper}>
+                    <img src={icon} alt=' ' width='20' height='20' />
+                </div>
+                <div className={classes.buttonTextWrapper}>{name}</div>
+            </Link>
+        )
     }
 
     return (
         <Navbar isDesktop={false} >
-            Fake
-            <img src={MenuIcon} alt=' ' width='35' height='35' onClick={() => { toggleDrawer(true) }} />
+            <div className={classes.logo}>IS</div>
+            <img src={MenuIcon} alt=' ' width='25' height='25' onClick={() => { toggleDrawer() }} />
             <Drawer
                 anchor={'right'}
                 open={opened}
-                onClose={() => { toggleDrawer(false) }}
+                onClose={() => { toggleDrawer() }}
             >
-                <div className={classes.options}>
-
+                <div className={classes.options} onClick={() => { toggleDrawer() }} >
+                {
+                    options.map((option, index) => {
+                        return button(index, option.path, option.name)
+                    })
+                }
+                <Divider classes={{root: classes.dividerRoot}} />
                 </div>
             </Drawer>
         </Navbar>
